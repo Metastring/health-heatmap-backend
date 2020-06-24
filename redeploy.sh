3a145f06517737152ba242d7735ffa68192dc023
Akshay S Dinesh <asdofindia@gmail.com>
2020-06-24 18:50:31 +0530

    refactor: remove obsolete deployment files

--- redeploy.sh
+++
@@ -1,8 +0,0 @@
-set -e
-systemctl --user stop hhm
-mvn compile package
-./scripts/drop-tables.sh
-./scripts/create-tables.sh
-java -jar target/data-reader-0.0.1.jar -p ../data/batchfile.csv -b
-java -jar target/data-reader-0.0.1.jar -p ../data/indicatorGroupings/exported.csv -d 'in' -t indicators
-systemctl --user start hhm
