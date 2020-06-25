4132526cccd99db39899f4870ab2bbbbf1553694
Akshay S Dinesh <asdofindia@gmail.com>
2020-06-25 21:21:03 +0530

    refactor: move generic data (multidata) to POST on data
    also rename "export" to say "dimensions"
    
    Swagger doesn't support mulitvaluedmap https://github.com/swagger-api/swagger-core/issues/2721

--- src/main/java/org/metastringfoundation/healthheatmap/web/resources/DataResource.java
+++ src/main/java/org/metastringfoundation/healthheatmap/web/resources/DataResource.java
@@ -16,23 +16,27 @@
 
 package org.metastringfoundation.healthheatmap.web.resources;
 
+import io.swagger.v3.oas.annotations.Operation;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.metastringfoundation.healthheatmap.logic.Application;
 import org.metastringfoundation.healthheatmap.logic.beanconverters.DataQueryResultToDataResponse;
 import org.metastringfoundation.healthheatmap.logic.beanconverters.DataRequestToDataQuery;
+import org.metastringfoundation.healthheatmap.logic.beanconverters.MultiMapToDataQuery;
 import org.metastringfoundation.healthheatmap.storage.beans.DataQueryResult;
 import org.metastringfoundation.healthheatmap.web.beans.DataRequest;
 import org.metastringfoundation.healthheatmap.web.beans.DataResponse;
 
 import javax.inject.Inject;
-import javax.ws.rs.BeanParam;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedHashMap;
 import java.io.IOException;
 
 @Path("data")
 public class DataResource {
+    private static final Logger LOG = LogManager.getLogger(DataResource.class);
+
     private final Application app;
 
     @Inject
@@ -44,8 +48,25 @@ public class DataResource {
     @Produces(MediaType.APPLICATION_JSON)
     public DataResponse getData(
             @BeanParam DataRequest dataRequest
-            ) throws IOException {
+    ) throws IOException {
         DataQueryResult queryResult = app.query(DataRequestToDataQuery.convert(dataRequest));
+        return DataQueryResultToDataResponse.convert(queryResult);
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(description = "SWAGGERFAIL: https://github.com/swagger-api/swagger-core/issues/2721\n" +
+            "This endpoint actually takes in a MultivaluedMap<String, String> " +
+            "(ie, a map with a key which is a string and value which is an array of strings. " +
+            "Perhaps you can use curl to send such a JSON as POST body. " +
+            "The key should be dimensions like 'indicator', 'source', etc. " +
+            "Values should be the value of these dimensions as it appears in the dataset.")
+    public DataResponse getData(
+            MultivaluedHashMap<String, String> params
+    ) throws IOException {
+        LOG.debug(params);
+        DataQueryResult queryResult = app.query(MultiMapToDataQuery.convert(params));
         return DataQueryResultToDataResponse.convert(queryResult);
     }
 }
