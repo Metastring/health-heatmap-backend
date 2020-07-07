9ea2ac7899211580d5c798dd3ea4b76e874da40a
Akshay S Dinesh <asdofindia@gmail.com>
2020-07-07 22:56:40 +0530

    doc: add/remove javadoc comments public/private appropriately

--- src/main/java/org/metastringfoundation/healthheatmap/package-info.java
+++ src/main/java/org/metastringfoundation/healthheatmap/package-info.java
@@ -1,6 +1,12 @@
 /**
- * This is the backend for health heat map.
- *
- * The healt heat map requires {@link org.metastringfoundation.healthheatmap.logic}
+ * This is the backend for health heatmap.
+ * <p>
+ * It is written in a hexagonal architecture with clear separation of domains into:
+ * <ul>
+ *     <li>{@link org.metastringfoundation.healthheatmap.cli} - Any interaction through CLI</li>
+ *     <li>{@link org.metastringfoundation.healthheatmap.logic} - Application logic</li>
+ *     <li>{@link org.metastringfoundation.healthheatmap.storage} - Storage and database queries</li>
+ *     <li>{@link org.metastringfoundation.healthheatmap.web} - REST APIs</li>
+ * </ul>
  */
 package org.metastringfoundation.healthheatmap;
